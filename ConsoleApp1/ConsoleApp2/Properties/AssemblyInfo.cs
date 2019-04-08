9c03bf907f87e31c32f6ccb4b0af066efb5fc0f6
Ali Hassani <hasaniali647@gmail.com>
2019-04-08 23:39:01 +0430

    Add T2

--- ConsoleApp1/ConsoleApp2/Properties/AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp2/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c913a91f-76e1-4e8d-87bd-ea3a9c822536")]
+[assembly: Guid("feb4b606-dc63-450c-9d53-068d868f397b")]
 
 // Version information for an assembly consists of the following four values:
 //
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,23 +1,9 @@
 ﻿using System;
-class Hello
+class Date
 {
     static void Main(string[] args)
     {
-        int[] number = new int[10];
-        Console.WriteLine("Enter 10 numbers:");
-        for (int i = 0; i < 10; i++)
-        {
-            number[i] = Int32.Parse(Console.ReadLine());
-        }
-        int max = number[0];
-        for (int j = 1; j < 10; j++)
-        {
-            if (number[j] > max)
-                max = number[j];
-        }
-        Console.WriteLine("The largest number is {0} ", max);
+        Console.WriteLine("{0:yyy/MM/dd}", DateTime.Now);
         Console.ReadLine();
     }
-
 }
-
